66a23a7791582d8acd7ba8f9002688efbb014c69
itsubaki <itsubaki@users.noreply.github.com>
2017-07-04 21:51:42 +0900

    Update function_map

--- function_map.go
+++ function_map.go
@@ -17,3 +17,61 @@ func (f SumMapInt) Apply(event []Event) []Event {
 
 	return event
 }
+
+type SumMapFloat struct {
+	Name string
+	Key  string
+}
+
+func (f SumMapFloat) Apply(event []Event) []Event {
+	var sum float64
+	for _, e := range event {
+		sum = sum + e.MapFloatValue(f.Name, f.Key)
+	}
+
+	for _, e := range event {
+		e.Record["sum("+f.Name+":"+f.Key+")"] = sum
+	}
+
+	return event
+}
+
+type AverageMapInt struct {
+	Name string
+	Key  string
+}
+
+func (f AverageMapInt) Apply(event []Event) []Event {
+	sum := 0
+	for _, e := range event {
+		sum = sum + e.MapIntValue(f.Name, f.Key)
+	}
+	length := len(event)
+	avg := float64(sum) / float64(length)
+
+	for _, e := range event {
+		e.Record["avg("+f.Name+":"+f.Key+")"] = avg
+	}
+
+	return event
+}
+
+type AverageMapFloat struct {
+	Name string
+	Key  string
+}
+
+func (f AverageMapFloat) Apply(event []Event) []Event {
+	var sum float64
+	for _, e := range event {
+		sum = sum + e.MapFloatValue(f.Name, f.Key)
+	}
+	length := len(event)
+	avg := float64(sum) / float64(length)
+
+	for _, e := range event {
+		e.Record["avg("+f.Name+":"+f.Key+")"] = avg
+	}
+
+	return event
+}
--- function_map_test.go
+++ function_map_test.go
@@ -27,3 +27,81 @@ func TestSumMapInt(t *testing.T) {
 		}
 	}
 }
+
+func TestSumMapFloat(t *testing.T) {
+	m := make(map[string]interface{})
+	m["piyo"] = 12.3
+
+	event := []Event{}
+	event = append(event, NewEvent(MapEvent{"foobar", m}).New())
+	event = append(event, NewEvent(MapEvent{"foobar", m}).New())
+
+	f := SumMapFloat{"Map", "piyo"}
+	result := f.Apply(event)
+
+	var test = []struct {
+		index int
+		sum   float64
+	}{
+		{0, 24.6},
+		{1, 24.6},
+	}
+
+	for _, tt := range test {
+		if result[tt.index].Record["sum(Map:piyo)"] != tt.sum {
+			t.Error(result)
+		}
+	}
+}
+
+func TestAverageMapInt(t *testing.T) {
+	m := make(map[string]interface{})
+	m["piyo"] = 15
+
+	event := []Event{}
+	event = append(event, NewEvent(MapEvent{"foobar", m}).New())
+	event = append(event, NewEvent(MapEvent{"foobar", m}).New())
+
+	f := AverageMapInt{"Map", "piyo"}
+	result := f.Apply(event)
+
+	var test = []struct {
+		index int
+		avg   float64
+	}{
+		{0, 15.0},
+		{1, 15.0},
+	}
+
+	for _, tt := range test {
+		if result[tt.index].Record["avg(Map:piyo)"] != tt.avg {
+			t.Error(result)
+		}
+	}
+}
+
+func TestAverageMapFloat(t *testing.T) {
+	m := make(map[string]interface{})
+	m["piyo"] = 15.0
+
+	event := []Event{}
+	event = append(event, NewEvent(MapEvent{"foobar", m}).New())
+	event = append(event, NewEvent(MapEvent{"foobar", m}).New())
+
+	f := AverageMapFloat{"Map", "piyo"}
+	result := f.Apply(event)
+
+	var test = []struct {
+		index int
+		avg   float64
+	}{
+		{0, 15.0},
+		{1, 15.0},
+	}
+
+	for _, tt := range test {
+		if result[tt.index].Record["avg(Map:piyo)"] != tt.avg {
+			t.Error(result)
+		}
+	}
+}
