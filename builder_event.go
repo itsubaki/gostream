7ce5ca1b590ca78caa5ad5b50e2480b4bb348701
itsubaki <itsubaki@users.noreply.github.com>
2017-07-09 16:14:52 +0900

    Update builder_event

--- builder_event.go
+++ builder_event.go
@@ -1,5 +1,7 @@
 package gocep
 
+import "reflect"
+
 type EevntBuilder struct {
 }
 
@@ -7,6 +9,16 @@ func NewEventBuilder() *EevntBuilder {
 	return &EevntBuilder{}
 }
 
-func (b *EevntBuilder) Build() Window {
-	return &SimpleWindow{}
+func (b *EevntBuilder) Build() reflect.Type {
+	return reflect.StructOf(
+		[]reflect.StructField{
+			reflect.StructField{
+				Name: "Name",
+				Type: reflect.TypeOf(""),
+			},
+			reflect.StructField{
+				Name: "Record",
+				Type: reflect.TypeOf(make(map[string]interface{})),
+			},
+		})
 }
---
+++ builder_event_test.go
@@ -0,0 +1,24 @@
+package gocep
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestEventBuilder(t *testing.T) {
+	b := NewEventBuilder()
+	tp := b.Build()
+
+	rf := reflect.New(tp).Elem()
+	rf.Field(0).SetString("foobar")
+	rf.Field(1).Set(reflect.MakeMap(reflect.TypeOf(make(map[string]interface{}))))
+	rf.Field(1).SetMapIndex(reflect.ValueOf("foo"), reflect.ValueOf("bar"))
+	rf.Field(1).SetMapIndex(reflect.ValueOf("val"), reflect.ValueOf(123))
+
+	fmt.Println(rf)
+	fmt.Println(rf.FieldByName("Name"))
+	fmt.Println(rf.FieldByName("Record"))
+	fmt.Println(rf.FieldByName("Record").MapIndex(reflect.ValueOf("foo")))
+	fmt.Println(rf.FieldByName("Record").MapIndex(reflect.ValueOf("val")))
+}
