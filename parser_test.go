6555d3a4b1c43abd9356ec7cf28e3e2647094ddb
itsubaki <itsubaki@users.noreply.github.com>
2017-12-07 21:56:33 +0900

    Update

--- parser_test.go
+++ parser_test.go
@@ -1,8 +1,19 @@
 package gocep
 
-import (
-	"testing"
-)
+import "testing"
+
+func TestParserError(t *testing.T) {
+	p := NewParser()
+
+	q := "select * from MapEvent.length(10)"
+	_, err := p.Parse(q)
+	if err == nil {
+		t.Error("failed.")
+	}
+	if err.Error() != "EventType [MapEvent] is not registered." {
+		t.Error("failed.")
+	}
+}
 
 func TestParser(t *testing.T) {
 	p := NewParser()
