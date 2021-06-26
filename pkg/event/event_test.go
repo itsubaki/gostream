bbe08121bd653aa1923f9ed3179adcd051c9c954
itsubaki <1759459+itsubaki@users.noreply.github.com>
2021-06-26 20:56:24 +0900

    Update some files

--- pkg/event/event_test.go
+++ pkg/event/event_test.go
@@ -6,49 +6,47 @@ import (
 	"github.com/itsubaki/gostream/pkg/event"
 )
 
-type IntEvent struct {
-	Name  string
-	Value int
-}
-
-type FloatEvent struct {
-	Name  string
-	Value float64
-}
-
-type BoolEvent struct {
-	Value bool
-}
-
-type MapEvent struct {
-	Record map[string]interface{}
-}
-
 func TestFloat(t *testing.T) {
-	event := event.New(FloatEvent{"foobar", 12.3})
+	type FloatEvent struct {
+		Name  string
+		Value float64
+	}
 
+	event := event.New(FloatEvent{"foobar", 12.3})
 	if event.Float("Value") != 12.3 {
 		t.Errorf("failed.")
 	}
 }
 
 func TestBool(t *testing.T) {
-	event := event.New(BoolEvent{true})
+	type BoolEvent struct {
+		Value bool
+	}
 
+	event := event.New(BoolEvent{true})
 	if !event.Bool("Value") {
 		t.Errorf("failed.")
 	}
 }
 
 func TestString(t *testing.T) {
-	event := event.New(IntEvent{"foobar", 123})
+	type IntEvent struct {
+		Name  string
+		Value int
+	}
 
+	event := event.New(IntEvent{"foobar", 123})
 	if event.String("Name") != "foobar" {
 		t.Errorf("failed.")
 	}
 }
 
 func TestRecordString(t *testing.T) {
+	type IntEvent struct {
+		Name  string
+		Value int
+	}
+
 	event := event.New(IntEvent{"foobar", 123})
 	event.Record["Name"] = "foobar"
 
