33a3503af830e819c2e6c847288c7f3dea740bf0
itsubaki <1759459+itsubaki@users.noreply.github.com>
2022-01-02 00:41:05 +0900

    Update parser

--- parser.go
+++ parser.go
@@ -1,6 +1,7 @@
 package gostream
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/itsubaki/gostream/lexer"
@@ -29,6 +30,22 @@ func (p *Parser) Errors() []error {
 	return p.errors
 }
 
+func (p *Parser) error(e error) {
+	p.errors = append(p.errors, e)
+}
+
+func (p *Parser) expect(t lexer.Token) {
+	if p.cursor.Token == t {
+		return
+	}
+
+	p.error(fmt.Errorf(
+		"got={Token:%v, Literal: %v}, want={Token:%v, Literal: %v}",
+		p.cursor.Token, p.cursor.Literal,
+		t, lexer.Tokens[t],
+	))
+}
+
 func (p *Parser) next() *Cursor {
 	token, literal := p.l.Tokenize()
 
@@ -41,24 +58,37 @@ func (p *Parser) next() *Cursor {
 	return p.cursor
 }
 
+func (p *Parser) parseAcceptType() interface{} {
+	return nil
+}
+
+func (p *Parser) parseLength() int {
+	return 10
+}
+
+func (p *Parser) parseTimeDuration() time.Duration {
+	return 10 * time.Minute
+}
+
 func Parse(l *lexer.Lexer) Stream {
 	s := NewStream()
-	p := NewParser(l)
 
+	p := NewParser(l)
 	p.next() // preload
 	for p.next().Token != lexer.EOF {
 		switch p.cursor.Token {
 		case lexer.SELECT:
 		case lexer.FROM:
-			s.Accept(Cursor{})
+			s.Accept(p.parseAcceptType())
 		case lexer.LENGTH:
-			s.Length(10)
+			s.Length(p.parseLength())
 		case lexer.LENGTH_BATCH:
-			s.LengthBatch(10)
+			s.LengthBatch(p.parseLength())
 		case lexer.TIME:
-			s.Time(10 * time.Minute)
+			s.Time(p.parseTimeDuration())
 		case lexer.TIME_BATCH:
-			s.TimeBatch(10 * time.Minute)
+			s.TimeBatch(p.parseTimeDuration())
+		case lexer.WHERE:
 		}
 	}
 
