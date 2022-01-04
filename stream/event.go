b1a627f9f326827eae8138d90d557c9e0e395eee
itsubaki <1759459+itsubaki@users.noreply.github.com>
2022-01-04 22:03:29 +0900

    Add json tag to Event

--- stream/event.go
+++ stream/event.go
@@ -3,9 +3,9 @@ package stream
 import "time"
 
 type Event struct {
-	Time       time.Time
-	Underlying interface{}
-	ResultSet  []interface{}
+	Time       time.Time     `json:"time"`
+	Underlying interface{}   `json:"underlying"`
+	ResultSet  []interface{} `json:"result_set"`
 }
 
 func NewEvent(input interface{}) Event {
